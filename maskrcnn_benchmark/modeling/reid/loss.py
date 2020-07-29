d1f28f67066c99bd878d49d35e3074e834366e94
hcc <hcc@hust.edu.cn>
2020-07-29 23:05:19 +1000

    7_27_5

--- maskrcnn_benchmark/modeling/reid/loss.py
+++ maskrcnn_benchmark/modeling/reid/loss.py
@@ -79,7 +79,7 @@ class OIMLossComputation(nn.Module):
 
         if 'sysu' in self.cfg.DATASETS.TRAIN[0]:
             self.num_pid = 5532
-            self.queue_size = 2048
+            self.queue_size = 1024
         elif 'prw' in self.cfg.DATASETS.TRAIN[0]:
             self.num_pid = 483
             self.queue_size = 500
