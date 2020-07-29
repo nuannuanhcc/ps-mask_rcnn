bd41f76e1c2557a339330a2ee4e240867e6a1b25
hcc <hcc@hust.edu.cn>
2020-07-29 11:30:59 +1000

    7_28_4

--- maskrcnn_benchmark/modeling/reid/loss.py
+++ maskrcnn_benchmark/modeling/reid/loss.py
@@ -128,7 +128,7 @@ class CIRCLELossComputation(nn.Module):
         self.cfg = cfg.clone()
 
         if 'sysu' in self.cfg.DATASETS.TRAIN[0]:
-            num_labeled = 512
+            num_labeled = 1024
             num_unlabeled = 0
         elif 'prw' in self.cfg.DATASETS.TRAIN[0]:
             num_labeled = 8192
