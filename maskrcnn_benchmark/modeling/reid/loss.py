3fc80ba9907b3a9dd48a96f8022f4225e9099213
hcc <hcc@hust.edu.cn>
2020-07-30 11:21:28 +1000

    7_30_1

--- maskrcnn_benchmark/modeling/reid/loss.py
+++ maskrcnn_benchmark/modeling/reid/loss.py
@@ -128,8 +128,8 @@ class CIRCLELossComputation(nn.Module):
         self.cfg = cfg.clone()
 
         if 'sysu' in self.cfg.DATASETS.TRAIN[0]:
-            num_labeled = 0
-            num_unlabeled = 2048
+            num_labeled = 3000
+            num_unlabeled = 0
         elif 'prw' in self.cfg.DATASETS.TRAIN[0]:
             num_labeled = 8192
             num_unlabeled = 8192
@@ -183,20 +183,19 @@ class CIRCLELossComputation(nn.Module):
         feat_labeled_k = features_k[aux_label_k > -1]
         feat_unlabeled_k = features_k[aux_label_k == -1]
 
-        # self.lut, _ = update_queue(self.lut, self.pointer[0], feat_labeled_k)
+        self.lut, _ = update_queue(self.lut, self.pointer[0], feat_labeled_k)
 
-        # self.id_inx, self.pointer[0] = update_queue(self.id_inx, self.pointer[0], id_labeled_k)
+        self.id_inx, self.pointer[0] = update_queue(self.id_inx, self.pointer[0], id_labeled_k)
         self.queue, self.pointer[1] = update_queue(self.queue, self.pointer[1], feat_unlabeled_k)
 
         queue_sim = torch.mm(feat_labeled, self.queue.t())
-        lut_sim = torch.mm(feat_labeled, feat_labeled_k.t())
-        positive_mask = id_labeled.view(-1, 1) == id_labeled_k.view(1, -1)
+        lut_sim = torch.mm(feat_labeled, self.lut.t())
+        positive_mask = id_labeled.view(-1, 1) == self.id_inx.view(1, -1)
         sim_ap = lut_sim.masked_fill(~positive_mask, float("inf"))
         sim_an = lut_sim.masked_fill(positive_mask, float("-inf"))
         sim_an = torch.cat((queue_sim, sim_an), dim=-1)
 
         pair_loss = circle_loss(sim_ap, sim_an)
-
         return pair_loss
 
 
